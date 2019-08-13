b5d2203e834e56b5919e190340c5132f2303ad8a
luoqiyi <499904394@qq.com>
2019-08-13 17:48:12 +0800

    change travis.yal

--- docker_push.sh
+++ docker_push.sh
@@ -2,8 +2,11 @@
 
 if [ $TRAVIS_TAG ];then
   image = "lonelyleaf/docker-registry-gc:$TRAVIS_TAG"
-  echo "start deploy $image"
-  docker build -t "$image"
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker push "$image"
+else
+  image = "lonelyleaf/docker-registry-gc"
 fi
+
+echo "start deploy $image"
+docker build -t "$image"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push "$image"
