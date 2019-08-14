4df97988fbaf2aa512c72d64825e255552856e0c
luoqiyi <499904394@qq.com>
2019-08-14 09:29:13 +0800

    change travis.yal

--- docker_push.sh
+++ docker_push.sh
@@ -7,6 +7,6 @@ else
 fi
 
 echo "start deploy $image"
-docker build -t "$image"
+docker build -t "$image" .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$image"
